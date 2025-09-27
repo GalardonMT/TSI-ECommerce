62b7266a25571d6738d7091f698b2fe25562be1e
ElPochero <cvivar@utem.cl>
2025-09-27 19:07:21 -0300

    Agregado .gitignore, requirements.txt, y cambio venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\pulga\OneDrive\Desktop\Proyectos universidad\Sistema de Informacion\TSI\TSI-ECommerce\proyecto-tsi\venv
+set VIRTUAL_ENV=C:\Proyecto TSI\TSI-ECommerce\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
