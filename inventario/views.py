45daa58cb425525535bd3c568b53a23286153508
ElPochero <cvivar@utem.cl>
2025-12-06 15:26:28 -0300

    Bloquear cambios realizados por no-staff

--- inventario/views.py
+++ inventario/views.py
@@ -5,9 +5,22 @@ from .models import Producto
 from .serializers import ProductoSerializer
 
 
+class IsStaffOrSuper(permissions.BasePermission):
+    """Allow access to staff or superusers."""
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated and (request.user.is_staff or request.user.is_superuser))
+
+
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all().order_by('nombre')
     serializer_class = ProductoSerializer
-    permission_classes = [permissions.AllowAny]
+
+    def get_permissions(self):
+        if self.action in ('create', 'update', 'partial_update', 'destroy'):
+            permission_classes = [permissions.IsAuthenticated, IsStaffOrSuper]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [perm() for perm in permission_classes]
 
 # Create your views here.
